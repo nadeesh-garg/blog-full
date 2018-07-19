cb835415bd924f14d6f3a60cb98aa052d6cce739
nadeesh-garg <nadeesh.garg@gmail.com>
2018-07-19 23:01:05 +0530

    Changes

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -12,7 +12,7 @@ class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
 	user = UserSerializer(required = False)
 	class Meta:
 		model = Profile
-		fields = ('url', 'user', 'pen_name', 'bio', 'birth_date', 'id', 'image')
+		fields = ('url', 'pen_name', 'bio', 'birth_date', 'id', 'image')
 	def create(self, validated_data):
 		user_data = validated_data.pop('user')
 		profile = Profile.objects.create(**validated_data)
